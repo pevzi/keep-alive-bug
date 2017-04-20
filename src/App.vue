b24cba5eb17b6bd4786c8e79e77881871a4f3716
Mikhail Sidorov <pevzi23@gmail.com>
2017-04-20 10:39:36 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <foo></foo>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import Foo from './Foo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Foo
+  }
+}
+</script>
---
+++ src/Foo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'foo',
+  data () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
